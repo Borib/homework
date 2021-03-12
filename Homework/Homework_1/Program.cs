8e4fcf3e633b8b7e6287b9cbf70e852f4e603ad6
Alexey Zhurkov <doxe@yandex.ru>
2021-03-12 23:01:33 +0300

    homework solved

--- Homework/Homework_1/Program.cs
+++ Homework/Homework_1/Program.cs
@@ -6,7 +6,31 @@ namespace Homework_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string userName = string.Empty;
+                Console.WriteLine("Please enter your name:");
+                userName = Console.ReadLine();                
+                while (string.IsNullOrEmpty(userName))
+                {
+                    Console.WriteLine($"You forgot to enter your name!{Environment.NewLine}Please try again:");
+                    userName = Console.ReadLine();                    
+                }
+                Console.WriteLine($"«Привет, {userName}, сегодня {DateTime.Now.ToShortDateString()}»");
+            }
+            #region errors handling
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"There is a few errors in console app:{Environment.NewLine}{ex.Message}{Environment.NewLine}{ex.InnerException.Message}");
+                }
+                else
+                {
+                    Console.WriteLine($"There is an error in console app:{Environment.NewLine}{ex.Message}");
+                }
+            }
+            #endregion
         }
     }
 }
